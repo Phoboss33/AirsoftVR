a5ffe2aa63220dd4c444d35de5512e580e410de6
Phoboss <102321702+Phoboss33@users.noreply.github.com>
2024-05-04 12:07:46 +0500

    die test

--- Assets/Scripts/BulletTrigger.cs
+++ Assets/Scripts/BulletTrigger.cs
@@ -1,12 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class BulletTrigger : MonoBehaviour
-{
+public class BulletTrigger : NetworkBehaviour {
+    public NetworkVariable<int> health = new NetworkVariable<int>();
+
+    private void Awake() {
+        health.Value = 3;
+    }
+
     private void OnTriggerEnter(Collider other) {
         if (other.tag == "Bullet") {
             Destroy(other.gameObject);
+            TakeDamage(1);
+        }
+    }
+
+    private void Update() {
+        if (health.Value <= 0) {
+            print("Умер");
+            Die();
+        }
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void TakeDamageServerRpc(int damage, ServerRpcParams rpcParams = default) {
+        health.Value -= damage;
+    }
+
+    private void TakeDamage(int damage) {
+        if (IsServer) {
+            health.Value -= damage;
+        }
+        else {
+            TakeDamageServerRpc(damage);
+        }
+    }
+
+    private void Die() {
+        if (IsServer) {
+            print($"{OwnerClientId} - Умер!!");
+            Destroy(gameObject);
+            NetworkObject.Despawn();
             
         }
     }
