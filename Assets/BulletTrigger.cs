f81a8a99e494ef6a18dea5b5ba377c6b5b6826de
Opirat <72846524+Ilya-svg-comm@users.noreply.github.com>
2024-05-06 20:02:29 +0500

    add die system

---
+++ Assets/BulletTrigger.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("SwatTest"))
+        {
+
+            other.gameObject.GetComponent<EliminateBeh>().InviseMePLZYouOkeyBroServerRpc();
+        }
+        if (other.gameObject.CompareTag("Player"))
+        {
+
+            other.gameObject.GetComponent<PlayerInGameBehavior>().InviseMe();
+        }
+    }
+}
